b5275d203617a7f92aa7d2f7bba40effeaa314b1
Konstantin <konstantin@revimedia.com>
2022-06-29 22:54:34 +0300

    Init commmit

---
+++ src/main/java/com/graphql/data/GraphqlDataDslApplication.java
@@ -0,0 +1,13 @@
+package com.graphql.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlDataDslApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqlDataDslApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/graphql/data/GraphqlDataDslApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graphql.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlDataDslApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
